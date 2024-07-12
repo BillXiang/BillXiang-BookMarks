82132c0e860d467410a705e70337e07cb63cb617
BillXiang <xiangwencheng@gmail.com>
2024-07-12 20:51:59 +0800

    Update ls.sh

--- ls.sh
+++ ls.sh
@@ -4,6 +4,10 @@
 #        echo "$file"
 #    fi
 #done
-echo "<!DOCTYPE html> <html> <body>" > index.html
-find . -type f -print0|xargs -0 -i echo {}|cut -d'/' -f2-|grep -v ".git"|grep -v ls.sh|awk '{print "<a href=\"https://billxiang.github.io/BillXiang-BookMarks/"$0"\"/>"}' >> index.html
+echo "<!DOCTYPE html> 
+<html> 
+  <head>
+  </head>
+<body>" > index.html
+find . -type f -print0|xargs -0 -i echo {}|cut -d'/' -f2-|grep -v ".git"|grep -v ls.sh|awk '{print "<a href=\"https://billxiang.github.io/BillXiang-BookMarks/"$0"\"></a>"}' >> index.html
 echo "</html> </body>" >> index.html
