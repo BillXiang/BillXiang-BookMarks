54fd95520c0e0f03c577c066bfd9742e9ba75a1b
BillXiang <xiangwencheng@gmail.com>
2024-12-18 16:47:18 +0800

    fix tags

--- web_deploy/ls.sh
+++ web_deploy/ls.sh
@@ -70,14 +70,14 @@ read_dir(){
                 echo $file | awk -F'[/()]' '{print $(NF-1), $(NF-2)}' | while read a b c
                 do
                     echo $a,$b,$c
-                    tags=$(echo $file|awk -F'[/]' '{$NF="";print $0}')
+                    tags=$(echo $file|awk -F'[/]' '{$NF="";for (i=1;i<=NF;i++) {if ($i=="书签工具栏"||$i=="study"||$i==".") {$i=""}};print $0}')
                     kimi=`cat "${file}.kimi"`
                     cat "${file}.kimi"
                     echo "<tr><td><table style='margin-top: 20px;margin-bottom: 20px;width:80%;'><tbody> \
                     <tr><td>${a}_${b}</td></tr> \
                     <tr style='font-size: 25px;'><td><a href=\"https://billxiang.github.io/BillXiang-BookMarks/$file\"><b>$c</b></a></td></tr> \
                     <tr><td>$kimi</td></tr> \
-                    <tr><td><a href='$ori_url'>原文链接</a></td><td>$tags</td></tr> \
+                    <tr><td><a href='$ori_url'>原文链接</a> TAGs:$tags</td></tr> \
                     </tbody></table></td></tr>" >> url.tmp
                     #echo "<tr></tr>" >> url.tmp
                 done
