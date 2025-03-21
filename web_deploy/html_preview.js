eba6df746f55294e739943e3dd5d29db9a8b60f1
BillXiang <xiangwencheng@gmail.com>
2025-03-21 17:02:05 +0800

    support preview of git raw html

---
+++ web_deploy/html_preview.js
@@ -0,0 +1,19 @@
+// 目标 raw.githubusercontent.com 的 HTML 文件 URL
+// const rawUrl = 'https://raw.githubusercontent.com/BillXiang/BillXiang-BookMarks/refs/heads/main/%E4%B9%A6%E7%AD%BE%E5%B7%A5%E5%85%B7%E6%A0%8F/%E8%99%9A%E6%8B%9F%E5%8C%96%26%E5%AE%B9%E5%99%A8/%E6%80%A7%E8%83%BD/KVM%E6%80%A7%E8%83%BD%E5%88%86%E6%9E%90%E5%B7%A5%E5%85%B7-zhurunguang-ChinaUnix%E5%8D%9A%E5%AE%A2%20(2025-03-07%2011%EF%BC%9A08%EF%BC%9A51).html';
+
+// 创建 URLSearchParams 对象
+const urlParams = new URLSearchParams(window.location.search);
+// 获取单个参数（返回第一个值）
+const rawUrl = urlParams.get('rawUrl'); // 例如：'John'
+// 发起跨域请求
+fetch(rawUrl)
+    .then(response => response.text())
+    .then(html => {
+    // 动态创建 iframe 并插入 HTML
+    const iframe = document.createElement('iframe');
+    document.body.appendChild(iframe);
+    iframe.srcdoc = html;
+    iframe.style.width = '100%';
+    iframe.style.height = '100vh';
+    })
+    .catch(error => console.error('Failed to load:', error));
