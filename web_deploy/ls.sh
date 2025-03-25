9087cb976a8a17cc1afe81ed4762cd929bdaa204
BillXiang <xiangwencheng@gmail.com>
2025-03-25 17:36:32 +0800

    add log for docs.tmp

--- web_deploy/ls.sh
+++ web_deploy/ls.sh
@@ -108,6 +108,8 @@ mkdir tags
 read_dir "."
 echo $html_head | tee all.html index.html docs.html
 
+cat docs.tmp
+
 find ./tags -type f |xargs -i mv {} {}.html
 echo "<hr><h3>TAGs</h3>" | tee -a all.html index.html docs.html
 wc -l tags/*|sort -r|tail -n +2|head -n 20|awk -F'[/.[:space:]]' '{print "<a href=./tags/"$(NF-1)".html>"$(NF-1),"</a>"$(NF-3)"篇<br>"}' | tee -a all.html index.html docs.html
