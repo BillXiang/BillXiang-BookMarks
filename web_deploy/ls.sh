8697b6f65bc37df71286a5a1604cccb0239a1d19
BillXiang <xiangwencheng@gmail.com>
2025-08-20 16:15:15 +0800

    Update ls.sh - change "contribute" to "Contribute"

--- web_deploy/ls.sh
+++ web_deploy/ls.sh
@@ -92,7 +92,7 @@ read_dir(){
                 echo $file | awk -F'[/()]' '{print $(NF-1), $(NF-2)}' | while read a b c
                 do
                     echo $a,$b,$c
-                    tags=$(echo $file|awk -F'[/]' '{for (i=1;i<NF;i++) {if ($i=="书签工具栏"||$i=="study"||$i==".") {$i=""} else {printf $i;if(i!=NF-1){printf " "}else{printf "\n"}}}}')
+                    tags=$(echo $file|awk -F'[/]' '{for (i=1;i<NF;i++) {if ($i=="contribute") {$i="Contribute"}; if ($i=="书签工具栏"||$i=="study"||$i==".") {$i=""} else {printf $i;if(i!=NF-1){printf " "}else{printf "\n"}}}}')
                     cd tags
                     echo "<tr><td><table style='margin-top: 20px;margin-bottom: 20px;width:80%;'><tbody> \
                     <tr><td style='display: none;'>_${a}_${b}_</td><td>${a} ${b}</td></tr> \
